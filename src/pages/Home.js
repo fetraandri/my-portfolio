e654bf54b782d7fb48f456306abfe4628f57063a
fetraandri <102484907+fetraandri@users.noreply.github.com>
2024-09-05 06:23:39 -0400

    fix: change a background image for home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,8 +4,8 @@ const Home = () => {
   return (
     <div className="background-image">
       <div className="container">
-        <h1 className="typewriter">Bienvenue sur Mon Portfolio</h1>
-        <p className="fade-in">Voici une introduction à mon site.</p>
+        <h1 className="typewriter">Je suis Fetra Tanjona, </h1>
+        <p className="fade-in">Un développeur frontend passionné par la création d'interfaces utilisateur intuitives et esthétiques. Avec une expertise en React et une maîtrise des technologies web modernes, je transforme des concepts en expériences interactives captivantes. Mon objectif est de construire des applications fluides et réactives qui répondent aux besoins des utilisateurs. J'apprécie les défis techniques et la résolution de problèmes complexes pour offrir des solutions élégantes.</p>
         <p className="zoom-text">Découvrez mon travail en parcourant les différentes sections.</p>
         <p className="move-text">Des animations professionnelles pour améliorer l'expérience utilisateur.</p>
       </div>
