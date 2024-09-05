e37c045b6949a1216c13c020365d2f85327a9428
fetraandri <102484907+fetraandri@users.noreply.github.com>
2024-09-05 07:12:02 -0400

    fix: a style text animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Home = () => {
+  useEffect(() => {
+    const aText = [
+      "Je suis Fetra Tanjona,"
+    ];
+    const iSpeed = 100; 
+    let iIndex = 0; // 
+    let iArrLength = aText[0].length; 
+    const iScrollAt = 20; 
+
+    let iTextPos = 0; 
+    let sContents = ''; 
+    let iRow; 
+
+    function typewriter() {
+      sContents = ' ';
+      iRow = Math.max(0, iIndex - iScrollAt);
+      const destination = document.getElementById("typedtext");
+      
+      while (iRow < iIndex) {
+        sContents += aText[iRow++] + '<br />';
+      }
+      destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+      if (iTextPos++ === iArrLength) {
+        iTextPos = 0;
+        iIndex++;
+        if (iIndex !== aText.length) {
+          iArrLength = aText[iIndex].length;
+          setTimeout(typewriter, 500);
+        }
+      } else {
+        setTimeout(typewriter, iSpeed);
+      }
+    }
+
+    typewriter(); 
+  }, []); 
+
   return (
     <div className="background-image">
       <div className="container">
-        <h1 className="typewriter">Je suis Fetra Tanjona, </h1>
-        <p className="fade-in">Un développeur frontend passionné par la création d'interfaces utilisateur intuitives et esthétiques. Avec une expertise en React et une maîtrise des technologies web modernes, je transforme des concepts en expériences interactives captivantes. Mon objectif est de construire des applications fluides et réactives qui répondent aux besoins des utilisateurs. J'apprécie les défis techniques et la résolution de problèmes complexes pour offrir des solutions élégantes.</p>
+        <div id="typedtext" className="typewriter-text"></div>
+        <p className="fade-in">Un développeur frontend passionné par la création d'interfaces utilisateur intuitives et esthétiques. Avec une expertise en React et une maîtrise des technologies web modernes, je transforme des concepts en expériences interactives captivantes. Mon objectif est de construire des applications fluides et réactives qui répondent aux besoins des utilisateurs.</p>
         <p className="zoom-text">Découvrez mon travail en parcourant les différentes sections.</p>
-        <p className="move-text">Des animations professionnelles pour améliorer l'expérience utilisateur.</p>
       </div>
     </div>
-
   );
 };
 
