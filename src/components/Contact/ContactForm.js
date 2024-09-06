814fabd161db7536315b9eaf92250a81d47e22dd
fetraandri <102484907+fetraandri@users.noreply.github.com>
2024-09-06 07:26:36 -0400

    feat: add a page contact with style and emailjs send

---
+++ src/components/Contact/ContactForm.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import '../../components/styles/ContactForm.css';
+import emailjs from 'emailjs-com';
+
+const ContactForm = () => {
+  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const serviceID = 'service_b22gwap';
+    const templateID = 'template_j8vqmoa';
+    const userID = 'iXEoJwv95GqkcnECZ';
+
+    const templateParams = {
+      from_name: formData.name,
+      from_email: formData.email,
+      message: formData.message,
+    };
+
+    emailjs.send(serviceID, templateID, templateParams, userID)
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+        setSubmitted(true);
+      }, (error) => {
+        console.error('FAILED...', error);
+      });
+  };
+
+  return (
+    <div className="contact-form-container">
+      <h2>Contactez-moi</h2>
+      {submitted ? (
+        <p>Merci pour votre message, je vous répondrai bientôt.</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Nom</label>
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">Message</label>
+            <textarea
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              required
+              className="form-control"
+              rows="5"
+            ></textarea>
+          </div>
+          <button type="submit" className="btn btn-primary">Envoyer</button>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default ContactForm;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,23 @@
+// pages/Contact.js
 import React from 'react';
+import ContactForm from '../components/Contact/ContactForm';
+import { SocialIcon } from 'react-social-icons';
+import './styles/Contact.css';
 
-function Contact() {
+const Contact = () => {
   return (
-    <div>
-      <h1>Contactez-moi</h1>
-      <p>Vous pouvez me contacter via ce formulaire.</p>
+    <div className="backgroundContact">
+      <div className="contact-page">
+        <ContactForm />
+        <div className="social-icons">
+        <SocialIcon url="https://github.com/fetraandri" target="_blank" className="mx-2" />
+          <SocialIcon url="https://linkedin.com/in/Fetra_Andriamamonjy" target="_blank" className="mx-2" />
+          <SocialIcon url="https://facebook.com/fetra.andriami.77" target="_blank" className="mx-2" />
+          <SocialIcon url="https://instagram.com/andriamamonjy_fetrra" target="_blank" className="mx-2" />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Contact;
