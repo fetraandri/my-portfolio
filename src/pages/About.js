2be805cf8762bd9356b1c9e4162eb05b4530463a
fetraandri <102484907+fetraandri@users.noreply.github.com>
2024-09-08 13:24:16 -0400

    feat: add skill bar in about.js page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,26 @@
 import React from 'react';
+import { useSpring, animated } from 'react-spring';
 import './styles/About.css';
 import profileImage from '../pages/styles/profil/profile.jpg';
+
+const SkillBar = ({ skill, percentage }) => {
+  const props = useSpring({
+    from: { width: '0%' },
+    to: { width: `${percentage}%` },
+    config: { duration: 1000 }
+  });
+
+  return (
+    <div className="skill">
+      <h3 className="progress-title">{skill}</h3>
+      <div className="progress">
+        <animated.div className="progress-bar" style={props}>
+          <div className="progress-value">{percentage}%</div>
+        </animated.div>
+      </div>
+    </div>
+  );
+};
 
 function About() {
   return (
@@ -14,9 +34,17 @@ function About() {
         <div className="text-section">
           <h1>À propos de moi</h1>
           <p>
-            Je suis étudiant en 3ème année chez HEI, où j'ai acquis des connaissances approfondies en INFORMATIQUE. Mes études m'ont fourni une solide base en programmation, que j'ai appliquée dans ma carrière professionnelle.
+          Je suis étudiant en 3ème année chez HEI, où j'ai acquis des connaissances approfondies en INFORMATIQUE. Mes études m'ont fourni une solide base en programmation, que j'ai appliquée dans ma carrière professionnelle.
           </p>
         </div>
+      </div>
+
+      <div className="skills">
+        <h2 className="skills-title">Compétences</h2>
+        <SkillBar skill="JavaScript" percentage={90} />
+        <SkillBar skill="React" percentage={85} />
+        <SkillBar skill="React Native" percentage={80} />
+        <SkillBar skill="Java Spring Boot" percentage={75} />
       </div>
     </div>
   );
